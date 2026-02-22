559dce1508e568ac6e1bc01c7fa269b9b39a60f9
KRISHNA AWASTHI <itskrishnaawasthi@gmail.com>
2026-02-23 00:46:51 +0530

    feat: implement basic class structure and object instantiation

---
+++ 04_OOPS/01_class_object.py
@@ -0,0 +1,20 @@
+'''class >>>> instance of object.
+     object >>>> anything that exits and has a name .
+        syntex >>> 
+         class class_name:
+              <statement>  '''
+
+class work():
+    var=1000
+
+s=work()
+print(s.var)
+
+class work():
+    var=1000
+    def display(self):
+        print(":days challenge:")
+
+s=work()
+print(s.var)
+s.display()
